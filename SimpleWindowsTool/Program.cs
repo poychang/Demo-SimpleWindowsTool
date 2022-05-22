5ca93b9c1baf7e59d0a843cf91b8bceec22c5b16
poychang <poypost@gmail.com>
2022-05-23 07:40:10 +0800

    update

--- SimpleWindowsTool/Program.cs
+++ SimpleWindowsTool/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿try
+{
+    var result = DoSomething();
+    ShowMessage($"SimpleWindowsTool say: {result}");
+}
+catch (Exception ex)
+{
+    ShowMessage(ex.Message);
+}
+
+void ShowMessage(string msg)
+{
+    System.Windows.Forms.MessageBox.Show(msg, "Message");
+}
+
+string DoSomething()
+{
+    // TODO: do something...
+    return "do something...";
+}
